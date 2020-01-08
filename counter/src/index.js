a8b37c97a5b6c65fef7623805b2e6a20d33dafd8
hsimao <e087754958@gmail.com>
2020-01-08 23:57:11 +0800

    add: [counter] - app function

--- counter/src/index.js
+++ counter/src/index.js
@@ -5,11 +5,11 @@ const { div, button } = hh(h)
 
 const initModel = 0
 
-function view(model) {
+function view(dispatch, model) {
   return div({ className: 'mv2' }, [
     div(`Count: ${model}`),
-    button({ className: 'pv1 ph2 mr2', onclick: () => console.log('+') }, '+'),
-    button({ className: 'pv1 ph2', onclick: () => console.log('-') }, '-'),
+    button({ className: 'pv1 ph2 mr2', onclick: () => dispatch('plus') }, '+'),
+    button({ className: 'pv1 ph2', onclick: () => dispatch('minus') }, '-'),
   ])
 }
 
@@ -24,6 +24,18 @@ function update(msg, model) {
   }
 }
 
+function app(initModel, update, view, node) {
+  let model = initModel
+  let currentView = view(dispatch, model)
+  node.appendChild(currentView)
+  function dispatch(msg) {
+    model = update(msg, model)
+    const updatedView = view(dispatch, model)
+    node.replaceChild(updatedView, currentView)
+    currentView = updatedView
+  }
+}
+
 const rootNode = document.getElementById('app')
 
-rootNode.appendChild(view(update('minus', initModel)))
+app(initModel, update, view, rootNode)
