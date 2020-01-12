a0ec456a67c00fd477a53a005bfd4e29e4f7a45a
hsimao <e087754958@gmail.com>
2020-01-12 14:53:51 +0800

    add: [卡路里] - 新增 meal、calories input 輸入更新事件, 同步 model data

--- caloriecounter/src/Update.js
+++ caloriecounter/src/Update.js
@@ -1,5 +1,9 @@
+import * as R from 'ramda'
+
 const MSGS = {
   SHOW_FORM: 'SHOW_FORM',
+  MEAL_INPUT: 'MEAL_INPUT',
+  CALORIES_INPUT: 'CALORIES_INPUT',
 }
 
 export function showFormMsg(showForm) {
@@ -9,11 +13,34 @@ export function showFormMsg(showForm) {
   }
 }
 
+export function mealInputMsg(description) {
+  return {
+    type: MSGS.MEAL_INPUT,
+    description,
+  }
+}
+
+export function caloriesInputMsg(calories) {
+  return {
+    type: MSGS.CALORIES_INPUT,
+    calories,
+  }
+}
+
 function update(msg, model) {
   switch (msg.type) {
     case MSGS.SHOW_FORM: {
       const { showForm } = msg
-      return { ...model, showForm }
+      return { ...model, showForm, description: '', calories: 0 }
+    }
+    case MSGS.MEAL_INPUT: {
+      const { description } = msg
+      return { ...model, description }
+    }
+    case MSGS.CALORIES_INPUT: {
+      // 將字串轉換成數字, 預設值為 0
+      const calories = R.pipe(parseInt, R.defaultTo(0))(msg.calories)
+      return { ...model, calories }
     }
   }
   return model
