2db3aa29da9af302348f57b657e57202d09fcb8a
hsimao <e087754958@gmail.com>
2020-01-14 20:28:32 +0800

    add: [溫度單位] - update input 方法

--- tempunitconvert_starter/src/Update.js
+++ tempunitconvert_starter/src/Update.js
@@ -1,7 +1,46 @@
-import * as R from 'ramda';
+import * as R from 'ramda'
 
-function update (msg, model) {
-  return model;
+export const MSGS = {
+  LEFT_VALUE_INPUT: 'LEFT_VALUE_INPUT',
+  RIGHT_VALUE_INPUT: 'RIGHT_VALUE_INPUT',
 }
 
-export default update;
+export function leftValueInputMsg(leftValue) {
+  return {
+    type: MSGS.LEFT_VALUE_INPUT,
+    leftValue,
+  }
+}
+
+export function rightValueInputMsg(rightValue) {
+  return {
+    type: MSGS.RIGHT_VALUE_INPUT,
+    rightValue,
+  }
+}
+
+// 將字串轉成純數字
+const toInt = R.pipe(parseInt, R.defaultTo(0))
+
+function handleUpdateInput(type, msg, model) {
+  const sourceLeft = type === 'leftValue' ? true : false
+  if (msg[type] === '') {
+    return { ...model, sourceLeft, leftValue: '', rightValue: '' }
+  }
+  const value = toInt(msg[type])
+  return { ...model, sourceLeft, [type]: value }
+}
+
+function update(msg, model) {
+  switch (msg.type) {
+    case MSGS.LEFT_VALUE_INPUT: {
+      return handleUpdateInput('leftValue', msg, model)
+    }
+    case MSGS.RIGHT_VALUE_INPUT: {
+      return handleUpdateInput('rightValue', msg, model)
+    }
+  }
+  return model
+}
+
+export default update
--- tempunitconvert_starter/src/View.js
+++ tempunitconvert_starter/src/View.js
@@ -1,6 +1,7 @@
 import * as R from 'ramda'
 import hh from 'hyperscript-helpers'
 import { h } from 'virtual-dom'
+import { leftValueInputMsg, rightValueInputMsg } from './Update'
 
 const { div, h1, pre, input, select, option } = hh(h)
 
@@ -10,12 +11,13 @@ function unitOptions(selectedUnit) {
   return UNITS.map(unit => option({ value: unit, selected: selectedUnit === unit }, unit))
 }
 
-function unitSection(dispatch, unit, value) {
+function unitSection(dispatch, unit, value, inputMsg) {
   return div({ className: 'w-50 ma1' }, [
     input({
       type: 'text',
       className: 'db w-100 mv2 pa2 input-reset ba',
       value,
+      oninput: e => dispatch(inputMsg(e.target.value)),
     }),
     select(
       {
@@ -30,8 +32,8 @@ function view(dispatch, model) {
   return div({ className: 'mw6 center' }, [
     h1({ className: 'f2 pv2 bb' }, 'Temperature Unit Converter'),
     div({ className: 'flex' }, [
-      unitSection(dispatch, model.leftUnit, model.leftValue),
-      unitSection(dispatch, model.rightUnit, model.rightValue),
+      unitSection(dispatch, model.leftUnit, model.leftValue, leftValueInputMsg),
+      unitSection(dispatch, model.rightUnit, model.rightValue, rightValueInputMsg),
     ]),
     pre(JSON.stringify(model, null, 2)),
   ])
