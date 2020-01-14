ae71b7a1d457e252b916ecc5bc51c19dd153f55b
hsimao <e087754958@gmail.com>
2020-01-14 21:04:31 +0800

    add: [溫度單位] - 單位轉換方法

--- tempunitconvert_starter/src/Update.js
+++ tempunitconvert_starter/src/Update.js
@@ -44,7 +44,7 @@ function handleUpdateInput(type, msg, model) {
     return { ...model, sourceLeft, leftValue: '', rightValue: '' }
   }
   const value = toInt(msg[type])
-  return { ...model, sourceLeft, [type]: value }
+  return convert({ ...model, sourceLeft, [type]: value })
 }
 
 function update(msg, model) {
@@ -57,14 +57,72 @@ function update(msg, model) {
     }
     case MSGS.LEFT_UNIT_CHANGED: {
       const { leftUnit } = msg
-      return { ...model, leftUnit }
+      return convert({ ...model, leftUnit })
     }
     case MSGS.RIGHT_UNIT_CHANGED: {
       const { rightUnit } = msg
-      return { ...model, rightUnit }
+      return convert({ ...model, rightUnit })
     }
   }
   return model
+}
+
+// 單位轉換方法
+function convert(model) {
+  const { leftValue, leftUnit, rightValue, rightUnit } = model
+
+  const [fromUnit, fromTemp, toUnit] = model.sourceLeft
+    ? [leftUnit, leftValue, rightUnit]
+    : [rightUnit, rightValue, leftUnit]
+
+  const otherValue = R.pipe(convertFromToTemp, round)(fromUnit, toUnit, fromTemp)
+
+  return model.sourceLeft ? { ...model, rightValue: otherValue } : { ...model, leftValue: otherValue }
+}
+
+function convertFromToTemp(fromUnit, toUnit, temp) {
+  // 找到對應轉換單位方法
+  // UnitConversions.Celsius.Fahrenheit
+  const convertFn = R.pathOr(R.identity, [fromUnit, toUnit], UnitConversions)
+  return convertFn(temp)
+}
+
+function round(number) {
+  return Math.round(number * 10) / 10
+}
+
+function FtoC(temp) {
+  return (5 / 9) * (temp - 32)
+}
+
+function CtoF(temp) {
+  return (9 / 5) * temp + 32
+}
+
+function KtoC(temp) {
+  return temp - 273.15
+}
+
+function CtoK(temp) {
+  return temp + 273.15
+}
+
+const FtoK = R.pipe(FtoC, CtoK)
+const KtoF = R.pipe(KtoC, CtoF)
+
+const UnitConversions = {
+  Celsius: {
+    Fahrenheit: CtoF,
+    Kelvin: CtoK,
+  },
+  Fahrenheit: {
+    Celsius: FtoC,
+    Kelvin: FtoK,
+  },
+  Kelvin: {
+    Celsius: KtoC,
+    Fahrenheit: KtoF,
+  },
 }
 
 export default update
