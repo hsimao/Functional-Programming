d31aba6daa16a378d3ba91e6013dccb12742e8f6
hsimao <e087754958@gmail.com>
2020-01-14 20:00:09 +0800

    add: [溫度單位] - unitSection View

--- tempunitconvert_starter/src/View.js
+++ tempunitconvert_starter/src/View.js
@@ -1,19 +1,40 @@
-import * as R from 'ramda';
-import hh from 'hyperscript-helpers';
-import { h } from 'virtual-dom';
+import * as R from 'ramda'
+import hh from 'hyperscript-helpers'
+import { h } from 'virtual-dom'
 
-const {
-  div,
-  h1,
-  pre,
-} = hh(h);
+const { div, h1, pre, input, select, option } = hh(h)
 
+const UNITS = ['Fahrenheit', 'Celsius', 'Kelvin']
+
+function unitOptions(selectedUnit) {
+  return UNITS.map(unit => option({ value: unit, selected: selectedUnit === unit }, unit))
+}
+
+function unitSection(dispatch, unit, value) {
+  return div({ className: 'w-50 ma1' }, [
+    input({
+      type: 'text',
+      className: 'db w-100 mv2 pa2 input-reset ba',
+      value,
+    }),
+    select(
+      {
+        className: 'db w-100 pa2 ba input-reset br1 bg-white ba b--black',
+      },
+      unitOptions(unit)
+    ),
+  ])
+}
 
 function view(dispatch, model) {
   return div({ className: 'mw6 center' }, [
     h1({ className: 'f2 pv2 bb' }, 'Temperature Unit Converter'),
+    div({ className: 'flex' }, [
+      unitSection(dispatch, model.leftUnit, model.leftValue),
+      unitSection(dispatch, model.rightUnit, model.rightValue),
+    ]),
     pre(JSON.stringify(model, null, 2)),
-  ]);
+  ])
 }
 
-export default view;
+export default view
